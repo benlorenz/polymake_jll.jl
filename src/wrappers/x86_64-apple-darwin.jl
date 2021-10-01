674c4b3217329f0bf6d19ce1b2f0f11ecf20568b
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2021-10-01 16:35:55 +0200

    polymake_jll build 400.500.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -2,24 +2,25 @@
 export generate_deps_tree, libpolymake, libpolymake_apps_rt, polymake, polymake_config
 
 using CompilerSupportLibraries_jll
-using FLINT_jll
 using GMP_jll
 using MPFR_jll
+using FLINT_jll
 using PPL_jll
 using Perl_jll
 using bliss_jll
 using boost_jll
 using cddlib_jll
+using lib4ti2_jll
 using lrslib_jll
 using normaliz_jll
 JLLWrappers.@generate_wrapper_header("polymake")
 JLLWrappers.@declare_file_product(generate_deps_tree)
-JLLWrappers.@declare_library_product(libpolymake, "@rpath/libpolymake.4.2.dylib")
-JLLWrappers.@declare_library_product(libpolymake_apps_rt, "@rpath/libpolymake-apps-rt.4.2.dylib")
+JLLWrappers.@declare_library_product(libpolymake, "@rpath/libpolymake.4.5.dylib")
+JLLWrappers.@declare_library_product(libpolymake_apps_rt, "@rpath/libpolymake-apps-rt.4.5.dylib")
 JLLWrappers.@declare_executable_product(polymake)
 JLLWrappers.@declare_executable_product(polymake_config)
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, FLINT_jll, GMP_jll, MPFR_jll, PPL_jll, Perl_jll, bliss_jll, boost_jll, cddlib_jll, lrslib_jll, normaliz_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, GMP_jll, MPFR_jll, FLINT_jll, PPL_jll, Perl_jll, bliss_jll, boost_jll, cddlib_jll, lib4ti2_jll, lrslib_jll, normaliz_jll)
     JLLWrappers.@init_file_product(
         generate_deps_tree,
         "share/polymake/generate_deps_tree.jl",
@@ -27,13 +28,13 @@ function __init__()
 
     JLLWrappers.@init_library_product(
         libpolymake,
-        "lib/libpolymake.4.2.dylib",
+        "lib/libpolymake.4.5.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libpolymake_apps_rt,
-        "lib/libpolymake-apps-rt.4.2.dylib",
+        "lib/libpolymake-apps-rt.4.5.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
